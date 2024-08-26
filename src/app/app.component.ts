425942ee21d3fe5a48bb2231b655423b304ff5f4
Silue Namogoh <namogoh.silue@st2i.net>
2024-08-26 17:52:17 +0000

    api consommé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'front-auth-scoring';
+  @ViewChild('login-body') headerElement!: ElementRef;
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, AppComponent, CommonModule, RouterOutlet],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  errorMessage: string | null = null;
+  http = inject(HttpClient);
+  loginForm = new FormGroup({
+    username: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+    password: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+  });
 
+  constructor(private router: Router) {}
+
+  loginButton() {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Veuillez remplir tous les champs avant de continuer.';
+    } else {
+      this.errorMessage = null;
+      
+      // login(credentials: AuthCredentials): Observable<AuthResponse> {
+      //   const headers = new HttpHeaders({
+      //     "Content-Type": "application/x-www-form-urlencoded",
+      //     accept: "application/json",
+      //   });
+      //   const body = new HttpParams()
+      //     .set("username", credentials.username)
+      //     .set("password", credentials.password);
+    
+      //   return this.http.post<AuthResponse>(
+      //     ${this.apiUrl}/login,
+      //     body.toString(),
+      //     { headers }
+      //   );
+      // }
+
+
+      // Créer les headers personnalisés
+      const headers = new HttpHeaders({
+        "Content-Type": "application/x-www-form-urlencoded",
+        accept: "application/json",
+      });
+
+     
+      const body = new HttpParams()
+          .set("username", this.loginForm.getRawValue().username)
+          .set("password", this.loginForm.getRawValue().password);
+
+      this.http.post("https://auth-scoring.onrender.com/api/v1/login", body.toString(), { headers }).subscribe({
+        next: (res: any) => {
+          console.log("**************:", res);
+          this.router.navigateByUrl("/dashboard");
+        },
+        error: (err) => {
+          console.log(err);
+          this.errorMessage = "Erreur lors de la connexion. Veuillez vérifier vos informations.";
+        }
+      });
+    }
+  }
 }
+  
